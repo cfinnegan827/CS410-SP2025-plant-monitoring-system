f9f6b089d3be3adc114c416eed4076bf28459b35
e-charles <evensoncharles18@gmail.com>
2025-04-11 20:47:33 -0400

    security bug fix

--- routes/posts.js
+++ routes/posts.js
@@ -4,13 +4,7 @@ import userModel from '../models/Users.js';
 const router = express.Router();
 
 router.post('/register', (req, res) => {
-    userModel.create({
-        //test
-        "name": "Jim Hefferon",
-        "username": "Jim",
-        "email": "jim123@gmaa.com",
-        "password": "password1"
-    })
+    userModel.create(req.body)
     .then(user => res.json(user))
     .catch(err => res.json(err))
     console.log('post route and user created hahahahah');
