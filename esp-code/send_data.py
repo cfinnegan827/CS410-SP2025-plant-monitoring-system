662e47e5745f8131d4a91846c2298f7d6ff0d3f1
Connor Finnegan <cfinnegan827@gmail.com>
2025-05-01 12:22:20 -0400

    add esp route model

--- esp-code/send_data.py
+++ esp-code/send_data.py
@@ -2,7 +2,7 @@ import urequests
 import json
 
 def send_data_db(data):
-    url = 'https://node_server'
+    url = f"https://node_server/environment/{env_id}/readings"
     headers = {"Content-Type": "application/json"}
     response = urequests.post(url, headers=headers, data=json.dumps(data))
     print(response.status_code)
--- esp-code/test.py
+++ esp-code/test.py
@@ -1,6 +1,6 @@
-import Temperature_Humidity as TR
-import Light_Intensity as LI
-import Soil_Moisture as SM
+import temperature_reading as TR
+import light_intensity as LI
+import soil_moisture as SM
 from send_data import send_data_db
 import time
 import network
@@ -9,14 +9,15 @@ import Connect
 
 
 def main():
-    if Connect.connect_wifi():
-        print("Main program start here!")
-        while True:
-            temp, humidity = TR.get_temp()
-            light_intensity = LI.read_light()
-            soil_moisture= SM.read_soil_moisture()
-            data = [temp, humidity, light_intensity, soil_moisture]
-            send_data_db(data)
-            time.sleep(3600)
+    #if Connect.connect_wifi():
+    print("Main program start here!")
+    while True:
+        temp, humidity = TR.get_temp()
+        light_intensity = LI.read_light()
+        soil_moisture= SM.read_soil_moisture()
+        data = [temp, humidity, light_intensity, soil_moisture]
+        #send_data_db(data)
+        time.sleep(2)
 
 main()
+
